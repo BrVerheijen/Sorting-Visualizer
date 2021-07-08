0e70697d11f55bd4c275fbc201a0bace5db2c482
Bram Verheijen <2063119verheijen@zuyd.nl>
2021-07-08 16:03:40 +0200

    Copy from repos

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import {getMergeSortAnimations} from '../SortingAlgorithms/SortingAlgorithms.js';
 import './SortingVisualizer.css';
 
 const PRIMARY_COLOR = 'turquoise';
+
+const SECONDARY_COLOR = 'red';
+
+const ANIMATION_SPEED_MS = 2;
 
 const NUMBER_OF_ARRAY_BARS = 100;
 
@@ -32,6 +36,27 @@ export default class SortingVisualizer extends React.Component {
     }
 
     MergeSort() {
+    const animations = getMergeSortAnimations(this.state.array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName('array-bar');
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
 
     }
 
